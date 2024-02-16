939c9726b193fafbc7a9947880a3921bfbc2cd09
Mari Mkrtchyan <137413787+mariocroix@users.noreply.github.com>
2024-02-16 12:39:12 +0400

    Update main.cpp

--- Probation_Task/main.cpp
+++ Probation_Task/main.cpp
@@ -1,6 +1,10 @@
 #include "TextAnalyzer.h"
 
+// Overall, the code is well-structured and organized, the application of STL containers is praiseworthy. 
+// Start to use <regex> library as well. Well done!
+
 int main() {
+    // use try catch here to catch exceptions for better performance
     TextAnalyzer analyzer("input.txt", "database.txt");
     analyzer.generateJSON("data.json");
 
